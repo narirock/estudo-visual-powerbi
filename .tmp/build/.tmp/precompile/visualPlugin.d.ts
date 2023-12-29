24cdf1a8d35196fb52b6df63f8a6f52d519f4d30
Renato Torres <renato.grangeiro@tjsp.jus.br>
2023-12-29 01:56:26 -0300

    wip

--- .tmp/build/.tmp/precompile/visualPlugin.d.ts
+++
@@ -1,4 +0,0 @@
-import powerbiVisualsApi from "powerbi-visuals-api";
-import IVisualPlugin = powerbiVisualsApi.visuals.plugins.IVisualPlugin;
-declare var gmtest1470B9908294B4C05B0E91F8FF920CAF7_DEBUG: IVisualPlugin;
-export default gmtest1470B9908294B4C05B0E91F8FF920CAF7_DEBUG;
