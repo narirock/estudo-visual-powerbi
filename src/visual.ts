41a8969181b6423884023cb2c3d29682865e6606
Renato Torres <nari9rock@hotmail.com>
2024-02-01 16:23:59 -0300

    wip

--- src/visual.ts
+++ src/visual.ts
@@ -15,12 +15,10 @@ const ptBR = formatLocale({
   decimal: ",",
   thousands: ".",
   grouping: [3],
-  currency: ["R$ ", ""],
+  currency: ["", ""],
 });
 
-const f = ptBR.format("($,.0f");
-
-
+const f = ptBR.format("-$,.0f");
 
 export class Visual implements IVisual {
   private body: Selection<HTMLBodyElement, any, any, any>;
@@ -31,38 +29,41 @@ export class Visual implements IVisual {
     console.log("created");
   }
 
-
   // eslint-disable-next-line max-lines-per-function
   public update(options: VisualUpdateOptions) {
-    const headerData = [{ value: options.dataViews[0].matrix.rows.levels[0].sources[0].displayName }, ...options.dataViews[0].matrix.columns.root.children];
-    headerData.pop();
-    headerData.push({ value: 'Total' });
-    headerData.push({ value: '%' });
 
     this.body.html("");
-
     this.table = this.body.append("table")
       .style('border-collapse', 'collapse')
-      .classed('table', true)
+      .classed('table', true);
 
-    this.table.append("thead")
-      .append("tr")
-      .selectAll("th")
-      .data(headerData)
-      .enter().append("th")
-      .style('position', 'sticky')
-      .text(function (d: any) {
-        return d.value;
-      })
-      .style('box-shadow', 'inset 0 -6px 0 -5px #FFAC00')
-      .style("padding", "0px 10px")
-      .style("font-size", "13px");
-
+    this.renderHeader(options);
     this.table.append('tbody')
-      .style('overflow', 'auto')
+      .style('overflow', 'auto');
 
-      ;
     this.renderTableBody(options.dataViews[0].matrix.rows.root.children, options);
+  }
+
+
+  private renderHeader(options: VisualUpdateOptions) {
+    try {
+      const headerData = [{ displayName: options.dataViews[0].matrix.rows.levels[0].sources[0].displayName }, ...options.dataViews[0].matrix.columns.levels[0].sources];
+      headerData.pop();
+      headerData.push({ displayName: 'Total' });
+      headerData.push({ displayName: '%' });
+
+      this.table.append("thead")
+        .append("tr")
+        .selectAll("th")
+        .data(headerData)
+        .enter().append("th")
+        .style('position', 'sticky')
+        .text(function (d: any) {
+          return d.displayName;
+        });
+    } catch (e) {
+      console.log(e);
+    }
   }
 
 
@@ -81,65 +82,70 @@ export class Visual implements IVisual {
     const lastRowValues = rows[rows.length - 1].values;
     const levelTotal = Object.values(lastRowValues).pop() as { value: number };
 
-
     rows.forEach((row: any) => {
+      console.log(row);
       if (row.isSubtotal && row.level > 0) return
+
       if (row.isSubtotal) {
         this.renderFooter(row, parentTotal || levelTotal.value);
         return;
       }
-      const subtotals = row.children?.find((r) => r.isSubtotal);
-      const values: { value: number }[] = subtotals ? Object.values(subtotals.values) : row.values ? Object.values(row.values) : [];
-      const currentTotal = values[values.length - 1]?.value || null;
-      if (parentTotal) {
-        const percent: number = (currentTotal || 0) * 100 / parentTotal;
-        values.push({ value: percent });
-      } else {
 
-        const percent: number = currentTotal * 100 / (levelTotal.value || 0);
-        values.push({ value: percent });
+      if (!row.value) return;
+
+      const subtotals = row.children?.find((r) => r.isSubtotal);
+      const values: any[] = subtotals ? Object.values(subtotals.values) : row.values ? Object.values(row.values) : [];
+      const currentTotal = values[values.length - 1]?.value || null;
+      let percent = 0;
+      if (parentTotal) {
+        percent = (currentTotal || 0) * 100 / parentTotal;
+      } else {
+        percent = currentTotal * 100 / (levelTotal.value || 0);
       }
+      values.push({ value: !isNaN(percent) ? percent : null });
+
       const tr = tbody
         .append('tr')
         .selectAll('td')
-        .data([{ value: row.isSubtotal ? 'Total' : row.value }, ...values])
+        .data([{ value: row.value }, ...values])
         .enter()
         .append('td')
         .html(function (d: any, index: number) {
-          if (index == values.length) return `${d.value.toFixed(0)} % `;
+          if (index == values.length && d.value != null) return `${d.value.toFixed(0)} % `;
           if (typeof d.value == 'number')
             return f(d.value);
 
-          if (index == 0)
-            return row.level > 0 ? `${'&nbsp;&nbsp;&nbsp;&nbsp;'.repeat(row.level)}<span>+</span>&nbsp${d.value}` : `<span>+</span>&nbsp<b>${d.value}</b>`
-
-
+          if (index == 0) {
+            const signal = row.isCollapsed ? "+" : "-";
+            return row.level > 0 ? `${'&nbsp;&nbsp;&nbsp;&nbsp;'.repeat(row.level)}<span>${signal}</span>&nbsp${d.value}` : `<span>${signal}</span>&nbsp<b>${d.value}</b>`
+          }
           return '';
         })
-        .style('padding', '0px 5px')
-        .style('font-size', '13.3333px')
         .style('text-wrap', 'nowrap')
         .style("color", function (d: { value: any }, index) {
-          if (typeof (d.value) == "number" && d.value < 0) {
-            return "red";
+          if (typeof (d.value) == "number") {
+            return d.value < 0 ? "red" : "#1A1F91";
           }
           return "black";
+        })
+        .style("font-family", function (_, index) {
+          return index > 0 ? "'Segoe UI Bold', wf_segoe-ui_bold, helvetica, arial, sans-serif" : "'Segoe UI Semibold', wf_segoe - ui_semibold, helvetica, arial, sans-serif"
+        })
+        .style("font-weight", function (_, index) {
+          return index != 0 ? "600" : "normal";
         })
         .style("text-align", function (d: { value: any }, index) {
           return typeof (d.value) == "number" ? "right" : "left";
         })
         .style('border-right', function (_, index) {
-          if (index == 0) return '1px solid #FFAC00';
-          return '';
-        });
-
+          return '1px solid #fff';
+        })
 
       if (row.children) {
         this.renderRows(row.children, options, tbody, currentTotal);
       }
     });
   }
-
 
   private renderFooter(row: any, parentTotal: number) {
 
@@ -165,13 +171,12 @@ export class Visual implements IVisual {
       .style('padding', function (d: any) {
         return typeof (d.value) == "number" ? '0px 5px' : '6px 6px 20px 6px'
       })
-      .style('font-size', '13.3333px')
       .style('text-wrap', 'nowrap')
       .style("color", function (d: { value: any }, index) {
         if (typeof (d.value) == "number" && d.value < 0) {
           return "red";
         }
-        return "black";
+        return "#1A1F91";
       })
       .style("text-align", function (d: { value: any }, index) {
         return typeof (d.value) == "number" ? "right" : "left";
